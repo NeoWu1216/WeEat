133a5197b442f17a2ffab6484fd08eff7e608220
Xavier Chi <chitianhaoxp@gmail.com>
2019-04-23 17:25:50 -0500

    setup mongodb connection

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
   token: "secret-starter-mern",
-  mongo_connection: "LINK HERE"
+  mongo_connection: "mongodb+srv://cs498rk:qwertyqwerty@cluster0-q9uap.mongodb.net/test?retryWrites=true"
 };
