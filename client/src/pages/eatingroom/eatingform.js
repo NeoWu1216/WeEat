70114ae71649c83be54beca8c99c1254f1a167e5
Le0198 <lopezleonardo198@gmail.com>
2019-05-02 22:14:09 -0500

    Cleaned EatingRoomList Styling

--- client/src/pages/eatingroom/eatingform.js
+++ client/src/pages/eatingroom/eatingform.js
@@ -1,52 +1,63 @@
 import React, { Component } from "react";
 import NavBar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
-import { withRouter } from 'react-router-dom'
-import { postNewRoom } from '../../api/eatingrooms'
-import { getMessage } from '../../api/parser'
-import "./eatingroom.scss";
+import { withRouter } from "react-router-dom";
+import { postNewRoom } from "../../api/eatingrooms";
+import { getMessage } from "../../api/parser";
+import styles from "./eatingroom.scss";
 
-class EatingForm extends Component{
+class EatingForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    	title: 'First post',
+      title: "First post",
       // date: '20:59', // start date
-      address:'Champaign', //meet point
-      restaurant:'Black Dog',
-      party_size: '1'
+      address: "Champaign", //meet point
+      restaurant: "Black Dog",
+      party_size: "1"
     };
     this.handleChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleInputChange = (e) => {
-    console.log(e.target.value)
-    this.setState({[e.target.id]:e.target.value})
-  }
+  handleInputChange = e => {
+    console.log(e.target.value);
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     /*url will be url of api*/
-    event.preventDefault()
-    event.stopPropagation()
+    event.preventDefault();
+    event.stopPropagation();
 
     // let now = new Date()
     // now.setHours(this.state.date.substr(0,2))
     // now.setMinutes(this.state.date.substr(3,2))
-    this.props.onSubmit({...this.state})
-  }
+    this.props.onSubmit({ ...this.state });
+  };
   render() {
-    const party='0, 1, 2, 3, 4, 5, 6, 7, 8'
-    const selectOptions = party.split(', ');
+    const party = "0, 1, 2, 3, 4, 5, 6, 7, 8";
+    const selectOptions = party.split(", ");
     const selectOptionsList = selectOptions.map((selectOption, index) => {
-      return <option key={index} value={index}>{selectOption}</option>
+      return (
+        <option key={index} value={index}>
+          {selectOption}
+        </option>
+      );
     });
 
     return (
-      <div id="form_outer">
-        <form onSubmit={this.handleSubmit} >
+      <div className="eating-room">
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="TitleInput">Room Name</label>
-            <input type="text" id="title" required value={this.state.title} onChange={this.handleChange} className="form-control" />
+            <input
+              type="text"
+              id="title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
           {/* <div className="form-group">
             <label htmlFor="TimeInput">Start From</label>
@@ -54,16 +65,35 @@ class EatingForm extends Component{
           </div> */}
           <div className="form-group">
             <label htmlFor="MeetInput">Meet At</label>
-            <input type="text" id="address" required value={this.state.address} onChange={this.handleChange} className="form-control"/>
+            <input
+              type="text"
+              id="address"
+              required
+              value={this.state.address}
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="ResInput">What to Eat?</label>
-            <input type="text" id="restaurant" required value={this.state.restaurant} onChange={this.handleChange} className="form-control" />
+            <input
+              type="text"
+              id="restaurant"
+              required
+              value={this.state.restaurant}
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="SizeInput">Party Size</label><br />
-            <select id="party_size" value={this.state.party_size} onChange={this.handleInputChange}>
-              <option value='' disabled></option>
+            <label htmlFor="SizeInput">Party Size</label>
+            <br />
+            <select
+              id="party_size"
+              value={this.state.party_size}
+              onChange={this.handleInputChange}
+            >
+              <option value="" disabled />
               {selectOptionsList}
             </select>
           </div>
@@ -71,8 +101,8 @@ class EatingForm extends Component{
           <input type="submit" value="Search" className="btn btn-primary" />
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default EatingForm
+export default EatingForm;
--- client/src/pages/eatingroom/eatingroom.js
+++ client/src/pages/eatingroom/eatingroom.js
@@ -1,127 +1,229 @@
 import React, { Component } from "react";
 import NavBar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
-import EatingForm from './eatingform'
-import { withRouter } from 'react-router-dom'
-import { getRooms, postMember } from "../../api/eatingrooms"
-import { getMessage } from "../../api/parser"
-import { getId } from '../../storage/id'
-import "./eatingroom.scss";
+import EatingForm from "./eatingform";
+import { withRouter } from "react-router-dom";
+import { getRooms, postMember } from "../../api/eatingrooms";
+import { getMessage } from "../../api/parser";
+import { getId } from "../../storage/id";
+import styles from "./eatingroom.scss";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
+import { Button } from "@material-ui/core";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { light: "#ffebe", main: "#fa5552" },
+    secondary: { main: "#ffebee" }
+  }
+});
 class EatingRoom extends Component {
   constructor(props) {
     super(props);
-    this.state = {eatingrooms:[], mounted: false};
+    this.state = { eatingrooms: [], mounted: false };
   }
 
   componentDidMount() {
     if (!this.state.mounted)
-    getRooms({}).then((data)=> {
-      this.setState({eatingrooms:data, mounted:true})
-    }).catch(err=>alert(getMessage(err)))
+      getRooms({})
+        .then(data => {
+          this.setState({ eatingrooms: data, mounted: true });
+        })
+        .catch(err => alert(getMessage(err)));
   }
 
-  onSubmit = (data) => {
-    getRooms(data).then((data)=> {
-      console.log(data)
-      this.setState({eatingrooms:data})
-    }).catch(err=>alert(getMessage(err)))
-  }
+  onSubmit = data => {
+    getRooms(data)
+      .then(data => {
+        console.log(data);
+        this.setState({ eatingrooms: data });
+      })
+      .catch(err => alert(getMessage(err)));
+  };
 
   render() {
     return (
-      <div id="eatingroom_outer">
-        <div id="eatingroom_background">
-          <NavBar />
-          <div id="eating_room_container">
-            <div id="filter_box">
-            </div>
-            <div className="eating_room_left">
-              <EatingRoomList eatingrooms={this.state.eatingrooms}/>
-              <button className="eating_room_button" onClick={()=>this.props.history.push('create-eatingroom')}>
+      <div className={styles.eatingroom}>
+        <NavBar />
+        <div className="eatingroom-bg">
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid
+              item
+              style={{
+                width: "65%",
+                height: "600px",
+                overflowY: "auto"
+              }}
+            >
+              <EatingRoomList eatingrooms={this.state.eatingrooms} />
+              {/* <button
+                className="eating_room_button"
+                onClick={() => this.props.history.push("create-eatingroom")}
+              >
                 Click me!
-              </button>
-            </div>
-            
-            <div className="eating_room_right">
-              <EatingForm onSubmit={this.onSubmit}/>
-            </div>
-
-          </div>
-          <Footer />
+              </button> */}
+            </Grid>
+            <Grid item>
+              <EatingForm onSubmit={this.onSubmit} />
+            </Grid>
+          </Grid>
         </div>
+        <Footer />
       </div>
     );
   }
 }
 
-
-
-
 class EatingRoomList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
-
   render() {
-    let result = this.props.eatingrooms
-    console.log(result)
+    let result = this.props.eatingrooms;
+    console.log(result);
     return (
-      <div id="eating_room_list_container">
-        {result.map((r, index) => <EatingRoomEntry r={r} key={index} />)}
+      <div className="eatingroom-list">
+        {result.map((r, index) => (
+          <EatingRoomEntry r={r} key={index} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
 class EatingRoomEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {room: this.props.r}
+    this.state = { room: this.props.r };
   }
   onJoin(e, _id) {
-    e.preventDefault()
-    e.stopPropagation()
-    postMember(_id).then((data)=> {
-      console.log('join',data)
-      this.setState({room: data})
-    }).catch(err=> {
-      alert(getMessage(err)) 
-      console.log(err.response)
-    })
+    e.preventDefault();
+    e.stopPropagation();
+    postMember(_id)
+      .then(data => {
+        console.log("join", data);
+        this.setState({ room: data });
+      })
+      .catch(err => {
+        alert(getMessage(err));
+        console.log(err.response);
+      });
   }
 
   render() {
-    let {room} = this.state
-    if (!room) return null
-    if ( room.participants===undefined) return null
-    if ( room.participants===null ) 
-      room.participants = []
+    let { room } = this.state;
+    if (!room) return null;
+    if (room.participants === undefined) return null;
+    if (room.participants === null) room.participants = [];
     return (
-      <div>
-        <div className="eating_room_card">
-          <div className="eating_room_card_left">
-            <img src={"https://s3-media1.fl.yelpcdn.com/bphoto/lJOnFchw-mzW6H5IdKh8Zg/o.jpg"} alt="img" />
-          </div>
-          <div className="eating_room_card_right">
-            <h1>{room.title}</h1>
-            <h1>{room.restaurant}</h1>
-            <h1>{room.address}</h1>
-            <h1>{room.date}</h1>
-
-            <div className="eating_room_card_right_details">
-              <ul>
-                {room.participants.map((uid)=>(<li key={uid}>{uid}</li>))}
-              </ul>
-              <div className="eating_room_button" onClick={(e)=>this.onJoin(e, room._id)}>
-                <a href="/">Join now!</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+      <MuiThemeProvider theme={theme}>
+        <Card
+          style={{
+            margin: "1em 2px",
+            width: "95%",
+            height: "250px"
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            // justify="center"
+            style={{
+              height: "100%"
+            }}
+          >
+            <Grid
+              item
+              style={{
+                height: "100%",
+                width: "35%"
+              }}
+            >
+              <CardMedia
+                title="Eating Room Image"
+                image={room.image_url}
+                style={{
+                  width: "100%",
+                  height: "100%"
+                }}
+              />
+            </Grid>
+            <Grid
+              item
+              style={{
+                height: "100%",
+                width: "65%",
+                padding: "0 1em"
+              }}
+            >
+              <CardHeader
+                title={room.title}
+                style={{
+                  textAlign: "center"
+                }}
+              />
+              <CardContent>
+                <div className="card-infolabel">
+                  <div className="card-label">
+                    <i class="fas fa-utensils" />:
+                  </div>
+                  {room.restaurant}
+                </div>
+                <div className="card-infolabel">
+                  <div className="card-label">
+                    <i class="fas fa-map-marker-alt" />:
+                  </div>
+                  {room.address}
+                </div>
+                <div className="card-infolabel">
+                  <div className="card-label">
+                    <i class="fas fa-calendar-day" />:
+                  </div>
+                  {room.date}
+                </div>
+                <Grid
+                  container
+                  justify="space-between"
+                  alignItems="center"
+                  style={{
+                    paddingTop: "1em"
+                  }}
+                >
+                  <Grid item style={{ width: "70%" }}>
+                    <div className="eatingroom-participants">
+                      {room.participants.map(uid => {
+                        if (uid === getId()) {
+                          return <i class="fas fa-user current-user" />;
+                        } else {
+                          return <i class="fas fa-user" />;
+                        }
+                      })}
+                    </div>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      onClick={e => this.onJoin(e, room._id)}
+                      color="primary"
+                    >
+                      Join
+                    </Button>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card>
+      </MuiThemeProvider>
+    );
   }
 }
 
